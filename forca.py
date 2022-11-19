e38cf6865543dbf5d6a389cdf141d61a857e98d1
lorenacarvalho <lonaca.paixao@gmail.com>
2022-11-18 23:30:21 -0300

    Update forca.py

--- forca.py
+++ forca.py
@@ -4,7 +4,23 @@ def jogar():
     print('{:*^50}'.format(' Bem vindo ao jogo da Forca! '))
     print("*" * 50)
 
-    print("Fim do jogo")
+    palavra_secreta = 'banana'
+    enforcou = False 
+    acertou = False
+    letras_acertadas = ["_ "] * len(palavra_secreta)
 
+    print(letras_acertadas)
+    while (not enforcou) and (not acertou):
+        chute = input('Tente uma letra?')
+        chute = chute.strip()
+
+        indice = 0
+        for letra in palavra_secreta:
+            if (chute.upper() == letra.upper()):
+                letras_acertadas[indice] = letra
+            indice += 1
+        print(letras_acertadas)
+
+print("Fim do jogo")
 if(__name__ == "__main__"):
     jogar()
